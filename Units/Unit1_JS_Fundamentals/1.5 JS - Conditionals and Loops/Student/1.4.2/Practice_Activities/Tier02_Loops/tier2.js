c418c8cf936ced0366ffc255e28eba0aaa2095a0
gcieza28077-glitch <gcieza28077@columbushs.com>
2025-11-18 14:09:28 -0500

    finish Tier 1

--- Units/Unit1_JS_Fundamentals/1.5 JS - Conditionals and Loops/Student/1.4.2/Practice_Activities/Tier02_Loops/tier2.js
+++ Units/Unit1_JS_Fundamentals/1.5 JS - Conditionals and Loops/Student/1.4.2/Practice_Activities/Tier02_Loops/tier2.js
@@ -1,7 +1,12 @@
-for(let n = 10; n >= 0; n--){
-     if(n == 0){
-         console.log("LIFTOFF")
-     } else{
-         console.log(n)
-     }
- }
+// for(let n = 10; n >= 0; n--){
+//      if(n == 0){
+//          console.log("LIFTOFF")
+//      } else{
+//          console.log(n)
+//      }
+//  }
+
+// for (let i = 1; i <= 10; i++) {
+//    console.log(i)
+// }
+
